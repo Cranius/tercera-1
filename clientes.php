a8d7ac4987eec1b1ef2bd3424ef820d579feeb99
Noel <noelsna@hotmail.com>
2025-10-15 08:02:23 -0600

    Creacion de Proyecto

--- clientes.php
+++ clientes.php
@@ -1,5 +1,5 @@
 <?php require_once "./vistas/vista_superior.php"?>
 
-
+Clientes
 
 <?php require_once "./vistas/vista_inferior.php"?>
---
+++ vistas/vista_superior.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Tercera 1">
+    <title>Tercera 1</title>
+    <link rel="stylesheet" href="./css/main.css">
+
+</head>
